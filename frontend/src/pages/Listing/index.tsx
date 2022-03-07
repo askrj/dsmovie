f1dd40377cefc6fee796468d00f08c63e46bb559
Alex Santos <ask.rj10@gmail.com>
2022-03-07 16:57:02 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,15 @@
+import MovieCard from "components/MovieCard";
+import MovieScore from "components/MovieScore";
 import Pagination from "components/pagination";
 
 function Listing(){
 
     return(
+
+        <>
         <Pagination></Pagination>
+        <MovieCard></MovieCard>
+        </>
     );
 }
 
