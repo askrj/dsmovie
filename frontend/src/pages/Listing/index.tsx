7f1072e465b6f5456fd6421c224bc04032bd7b4b
Alex Santos <ask.rj10@gmail.com>
2022-03-08 08:08:14 -0300

    movie cards estilazado

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -10,16 +10,22 @@ function Listing(){
 
         <div className="container">
             <div className="row">
-                <div className="col">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                 <MovieCard></MovieCard>
                 </div>
-                <div className="col">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                 <MovieCard></MovieCard>
                 </div>
-                <div className="col">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                 <MovieCard></MovieCard>
                 </div>
-                <div className="col">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                <MovieCard></MovieCard>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                <MovieCard></MovieCard>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                 <MovieCard></MovieCard>
                 </div>
             </div>
